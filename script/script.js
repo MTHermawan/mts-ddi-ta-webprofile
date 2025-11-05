bd603b94057d20af9908f1a2f169d566ebc7eded
MTHermawan <trigusnihermawan@gmail.com>
2025-11-06 01:11:12 +0800

    counting jumlat siswa, guru, lulusan

--- script/script.js
+++ script/script.js
@@ -1,32 +1,22 @@
-// // Deklarasi Variabel / Element
-// const siteHeader = document.querySelector('div.site-header');
-// const headerNavbar = document.querySelector('nav');
-
-// // Object
-// const observer = new IntersectionObserver(([entry]) => {
-//     if (entry.intersectionRatio > 0) {
-//         if (headerNavbar.classList.contains('stuck'))
-//             headerNavbar.classList.remove('stuck');
-//     }
-//     else {
-//         if (!headerNavbar.classList.contains('stuck'))
-//             headerNavbar.classList.add('stuck');
-//     }
-//     console.log(entry.intersectionRatio);
-// }, { threshold: [0] });
-
-
-// // Main
-// observer.observe(siteHeader);
-
 // Deklarasi Variabel / Element
+
+// Header
 const header = document.querySelector('header');
 const headerNavbar = document.querySelector('nav');
 const stickyNavbar = headerNavbar.cloneNode(true);
 const heroSection = document.getElementById('hero');
 
+const jumlahSiswaElement = document.getElementById('value_jumlah_siswa');
+const jumlahGuruElement = document.getElementById('value_jumlah_guru');
+const jumlahLulusanElement = document.getElementById('value_jumlah_lulusan');
+const jumlahSiswa = 720;
+const jumlahGuru = 64;
+const jumlahLulusan = 3500;
+const jumlahDataDuration = 2.5;
+
+
 // Object
-const observer = new IntersectionObserver(([entry]) => {
+const navbarObserver = new IntersectionObserver(([entry]) => {
     if (entry.intersectionRatio <= 0.99 && !document.body.contains(stickyNavbar)) {
         document.body.insertBefore(stickyNavbar, header);
         headerNavbar.style.opacity = '0';
@@ -37,12 +27,29 @@ const observer = new IntersectionObserver(([entry]) => {
     }
 }, { threshold: [0.99, 1.0] });
 
+const jumlahDataObserver = new IntersectionObserver(([entry]) => {
+    if (entry.isIntersecting && entry.intersectionRatio > 0.25) {
+        jumlahDataObserver.unobserve(jumlahSiswaElement);
+        
+        const startTime = performance.now();
+        intervalStopwatch = setInterval(() => {
+            const currentTime = performance.now();
+            const elapsedTimes = (currentTime - startTime) / 1000;
+            const progress = elapsedTimes / jumlahDataDuration;
+
+            jumlahSiswaElement.innerHTML = Math.floor(jumlahSiswa * Math.min(1, (progress / 1))) + "+";
+            jumlahGuruElement.innerHTML = Math.floor(jumlahGuru * Math.min(1, (progress / 1))) + "+";
+            jumlahLulusanElement.innerHTML = Math.floor(jumlahLulusan * Math.min(1, (progress / 1))) + "+";
+            if (progress > 1) clearInterval(intervalStopwatch);
+        }, 50);
+    }
+}, { threshold: [0.25] });
 
 // Main
-
 // Setting up cloned navbar
 stickyNavbar.id = '';
 stickyNavbar.classList.add('stuck');
 stickyNavbar.style.zIndex = '1000';
 
-observer.observe(headerNavbar);
+navbarObserver.observe(headerNavbar);
+jumlahDataObserver.observe(jumlahSiswaElement);
