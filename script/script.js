e6d40b82cc8c355fef1e2e52f693b1831ef701d5
M. Fahrianor <moraxz112@gmail.com>
2025-11-05 20:49:14 +0800

    perbaikan homepage

--- script/script.js
+++ script/script.js
@@ -1,20 +1,48 @@
+// // Deklarasi Variabel / Element
+// const siteHeader = document.querySelector('div.site-header');
+// const headerNavbar = document.querySelector('nav');
+
+// // Object
+// const observer = new IntersectionObserver(([entry]) => {
+//     if (entry.intersectionRatio > 0) {
+//         if (headerNavbar.classList.contains('stuck'))
+//             headerNavbar.classList.remove('stuck');
+//     }
+//     else {
+//         if (!headerNavbar.classList.contains('stuck'))
+//             headerNavbar.classList.add('stuck');
+//     }
+//     console.log(entry.intersectionRatio);
+// }, { threshold: [0] });
+
+
+// // Main
+// observer.observe(siteHeader);
+
 // Deklarasi Variabel / Element
-const siteHeader = document.querySelector('div.site-header');
+const header = document.querySelector('header');
 const headerNavbar = document.querySelector('nav');
+const stickyNavbar = headerNavbar.cloneNode(true);
+const heroSection = document.getElementById('hero');
 
 // Object
 const observer = new IntersectionObserver(([entry]) => {
-    if (entry.intersectionRatio > 0) {
-        if (headerNavbar.classList.contains('stuck'))
-            headerNavbar.classList.remove('stuck');
+    if (entry.intersectionRatio <= 0.99 && !document.body.contains(stickyNavbar)) {
+        document.body.insertBefore(stickyNavbar, header);
+        headerNavbar.style.opacity = '0';
     }
-    else {
-        if (!headerNavbar.classList.contains('stuck'))
-            headerNavbar.classList.add('stuck');
+    else if (document.body.contains(stickyNavbar)) {
+        document.body.removeChild(stickyNavbar);
+        headerNavbar.style.opacity = '1';
     }
-    console.log(entry.intersectionRatio);
-}, { threshold: [0] });
+}, { threshold: [0.99, 1.0] });
 
 
 // Main
-observer.observe(siteHeader);
+
+// Setting up cloned navbar
+stickyNavbar.id = '';
+stickyNavbar.classList.add('stuck');
+stickyNavbar.style.zIndex = '1000';
+
+observer.observe(headerNavbar);
