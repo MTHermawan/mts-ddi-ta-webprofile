0b23d9f27bdda34014e46875f5b50107bb00da60
M. Fahrianor <moraxz112@gmail.com>
2025-11-09 01:45:47 +0800

    perbaikan pada dashboard, penambahan font sytle, penambahan manajemen fitur

--- script/dashboard-admin.js
+++ script/dashboard-admin.js
@@ -1,13 +1,13 @@
-document.querySelectorAll(".menu-item").forEach((item) => {
-  item.addEventListener("click", function (e) {
-    e.preventDefault();
+// document.querySelectorAll(".menu-item").forEach((item) => {
+//   item.addEventListener("click", function (e) {
+//     e.preventDefault();
 
-    // Hapus class active dari semua menu items
-    document.querySelectorAll(".menu-item").forEach((i) => {
-      i.classList.remove("active");
-    });
+//     // Hapus class active dari semua menu items
+//     document.querySelectorAll(".menu-item").forEach((i) => {
+//       i.classList.remove("active");
+//     });
 
-    // Tambahkan class active ke menu item yang diklik
-    this.classList.add("active");
-  });
-});
+//     // Tambahkan class active ke menu item yang diklik
+//     this.classList.add("active");
+//   });
+// });
