0a2ab9ad1427ee0ffa1a8043c6de36eb339853b0
mfthfzn <miftahulfauzan55@gmail.com>
2025-11-08 21:24:09 +0700

    Fix Login Admin

--- admin/login-admin.php
+++ admin/login-admin.php
@@ -13,45 +13,73 @@ if ($_SERVER['REQUEST_METHOD'] === "POST" && isset($_POST['submit_login'])) {
     $akun = GetAdminByUsername($input_username);
     if ($akun && $akun['password'] == $input_password) {
         $_SESSION['email'] = $akun['email'];
-        header("Location: ./dashboard.php");
+        header("Location: ./dashboard-admin.php");
     }
 }
-
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<html lang="id">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width,initial-scale=1" />
     <title>Login</title>
-    <link rel="stylesheet" href="../style/login-admin.css">
-</head>
+    <link rel="stylesheet" href="../style/login-admin.css" />
+  </head>
 
-<body>
-    <section class="login-sec">
-        <div class="img">
-            <img src="../assets/admin.png" alt="admin">
+  <body>
+    <header class="header">
+      <div class="logo">
+        <img src="../assets/logo-sekolah.png" alt="logo MTs DDI TA" />
+        <p>MTs DDI Tani Aman</p>
+      </div>
+      <div></div>
+    </header>
+    <div class="container">
+      <div class="login-container">
+        <div class="image-container">
+          <img src="../assets/login.jpg" alt="logo login" />
         </div>
-        <div class="container">
-            <h2>Login</h2>
-
-            <form method="POST" action="">
-                <div class="input">
-                    <label for="input_username">Username</label>
-                    <input class="isi" type="text" name="username" id="input_username"
-                        value="<?php echo isset($_POST['username']) ? $_POST['username'] : ''; ?>">
-
-                    <label for="input_password">Password</label>
-                    <input class="isi" type="password" name="password" id="input_password">
-                </div>
-                <button name="submit_login" type="submit">Login</button>
-            </form>
+        <div class="form-container">
+          <h2 class="login-header">Login</h2>
+          <p class="login-description">
+            Masukkan email dan kata sandi untuk masuk ke akun Anda.
+          </p>
+          <form action="" method="POST" class="login-form">
+            <label class="email-label" for="email">Email</label>
+            <input
+              id="email"
+              name="email"
+              type="email"
+              required
+              placeholder="email@gmail.com"
+            />
+            <div class="password">
+              <label class="password-label" for="password">Kata Sandi</label>
+              <input
+                id="password"
+                name="password"
+                type="password"
+                required
+                placeholder="Masukkan kata sandi"
+              />
+              <img
+                class="visibility-logo"
+                src="../assets/hide.svg"
+                alt="logo show"
+              />
+            </div>
+            <div class="form-options">
+              <label> <input type="checkbox" /> Ingat saya </label>
+            </div>
+            <p class="message-error">
+              <?php echo htmlspecialchars($message); ?>
+            </p>
+            <button type="submit">Login</button>
+          </form>
         </div>
-    </section>
-
-    <script src="../script/script.js"></script>
-</body>
-
+      </div>
+    </div>
+  </body>
+  <script src="../script/login-admin.js"></script>
 </html>
