118c735ec5b354dd05c5efcd724d7e6a28615004
M. Fahrianor <moraxz112@gmail.com>
2025-11-10 07:39:24 +0800

    perbaikan

--- script/dashboard-admin.js
+++ script/dashboard-admin.js
@@ -12,43 +12,43 @@
 //   });
 // });
 
-const btnAddGuru = document.getElementById("btn_tambah_guru");
-const main = document.querySelector(".main");
+// const btnAddGuru = document.getElementById("btn_tambah_guru");
+// const main = document.querySelector(".main");
 
-btnAddGuru.addEventListener("click", () => {
-    alert(main);
-    document.append(```
-        <section class="popup">
-      <form class="popup-content">
-        <h1>Tambah Guru</h1>
-        <div class="popup-input-group">
-          <div class="popup-input">
-            <label for="nama">Nama Lengkap</label>
-            <input type="text" name="nama" id="nama">
-          </div>
-        </div>
-        <div class="popup-input-group">
-          <div class="popup-input">
-            <label for="mapel">Mapel</label>
-            <input type="text" name="mapel" id="gelar">
-          </div>
-          <div class="popup-input">
-            <label for="gelar">Gelar</label>
-            <input type="text" name="gelar" id="gelar">
-          </div>
-        </div>
-        <div class="popup-input-group">
-          <div class="popup-input-gambar">
-            <label for="foto_guru">
-              <img src="../assets/icon-tambah-gambar.svg" alt="">Tambah Gambar
-            </label>
-            <input type="file" name="foto_guru" id="foto_guru">
-          </div>
-          <button type="submit" class="popup-button-tambah">Tambah</button>
-          <button type="button" class="popup-button-kembali">Kembali</button>
-        </div>
-      </form>
-    </section>
-  </div>
-        ```);
-});
+// btnAddGuru.addEventListener("click", () => {
+//     alert(main);
+//     document.append(```
+//         <section class="popup">
+//       <form class="popup-content">
+//         <h1>Tambah Guru</h1>
+//         <div class="popup-input-group">
+//           <div class="popup-input">
+//             <label for="nama">Nama Lengkap</label>
+//             <input type="text" name="nama" id="nama">
+//           </div>
+//         </div>
+//         <div class="popup-input-group">
+//           <div class="popup-input">
+//             <label for="mapel">Mapel</label>
+//             <input type="text" name="mapel" id="gelar">
+//           </div>
+//           <div class="popup-input">
+//             <label for="gelar">Gelar</label>
+//             <input type="text" name="gelar" id="gelar">
+//           </div>
+//         </div>
+//         <div class="popup-input-group">
+//           <div class="popup-input-gambar">
+//             <label for="foto_guru">
+//               <img src="../assets/icon-tambah-gambar.svg" alt="">Tambah Gambar
+//             </label>
+//             <input type="file" name="foto_guru" id="foto_guru">
+//           </div>
+//           <button type="submit" class="popup-button-tambah">Tambah</button>
+//           <button type="button" class="popup-button-kembali">Kembali</button>
+//         </div>
+//       </form>
+//     </section>
+//   </div>
+//         ```);
+// });
