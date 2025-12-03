251c076ae13ea6fc28997b50f168975f68db9fed
M. Fahrianor <moraxz112@gmail.com>
2025-12-03 17:34:20 +0800

    update besar

---
+++ script/fade-in.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const items = document.querySelectorAll(".fade-in");
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("show");
+            }
+        });
+    }, {
+        threshold: 0.2
+    });
+
+    items.forEach(item => observer.observe(item));
+});
